9c174dbd3107a755039b3f8c623e07c007500512
Chengwen <chengwen.sui@gmail.com>
2014-09-13 16:41:04 -0400

    update

--- src/com/miracle/topdf/WebFragment.java
+++ src/com/miracle/topdf/WebFragment.java
@@ -1,8 +1,22 @@
 package com.miracle.topdf;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.RequestQueue;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
 import android.support.v4.app.Fragment;
 import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -18,6 +32,7 @@ public class WebFragment extends Fragment{
 	private EditText editText;
     private Button   button;
     private WebView  webView;
+    private RequestQueue mQueue;
     //String myUrl;
     
 	@SuppressLint("SetJavaScriptEnabled")
@@ -33,9 +48,43 @@ public class WebFragment extends Fragment{
         webSettings.setBuiltInZoomControls(true);
         webSettings.setJavaScriptEnabled(true);
         
+        mQueue = Volley.newRequestQueue(MenuActivity.mContext);  
+        
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-            		webView.loadUrl("http://"+editText.getText().toString());
+                    String url="http://"+editText.getText().toString();
+            		webView.loadUrl(url);
+            		
+            		String apiurl="";
+            		
+                    try {
+                      apiurl = "http://api.docs88.com/v1/webtopdf?url="+URLEncoder.encode(url, "UTF-8");
+                    } catch (UnsupportedEncodingException e1) {
+                      // TODO Auto-generated catch block
+                      e1.printStackTrace();
+                    }
+
+            		JsonObjectRequest jsonObjRequest = new JsonObjectRequest(Request.Method.GET,
+            		    apiurl, null, 
+            		                            new Response.Listener<JSONObject>() {
+            		    @Override
+            		    public void onResponse(JSONObject response) {
+            		      try {
+                            Log.d("status",String.valueOf(response.getInt("status"))+" "+response.getString("url"));
+                          } catch (JSONException e) {
+                            // TODO Auto-generated catch block
+                            e.printStackTrace();
+                          }
+            		    }
+            		}, new Response.ErrorListener() {
+
+            		    @Override
+            		    public void onErrorResponse(VolleyError error) {
+            		    }
+            		});
+
+            		mQueue.add(jsonObjRequest); 
+            		
             }
         });
         
